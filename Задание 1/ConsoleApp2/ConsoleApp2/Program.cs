60f82a2ee4f66c23591fbbe036e66894453630ea
Ilya Khrebtov(mrProger) <zkhrebtovilya@gmail.com>
2022-02-07 15:25:39 +0600

    Загружены результаты выполнения тестовых заданий

---
+++ Задание 1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        // Объявляем объект класса Random для генерации рандомных чисел
+        public static Random rand = new Random();
+
+        // Массив с числами для проверки
+        public static int[] numberArr = new int[10];
+
+        // Коллекция для хранения уникальных значений
+        public static List<int> uniqueList = new List<int>();
+
+        // Переменная для записи проверяемого числа
+        public static int num = 0;
+
+        // Счетчик вхождений проверяемого числа в массив
+        public static int count = 0;
+
+        static void Main(string[] args)
+        {
+            // Генерируем рандомные значения в массиве numberArr
+            for (int i = 0; i < numberArr.Length; i++)
+                numberArr[i] = rand.Next(0, 10);
+
+            /* Вызываем функцию getUniqueNumbers передавая numberArr как параметр
+            и ожидая возвращения значения в uniqueList*/
+            uniqueList = getUniqueNumbers(numberArr);
+
+            // Выводим все значения из массива numberArr
+            for (int i = 0; i < numberArr.Length; i++)
+                Console.WriteLine($"numberArr - {numberArr[i]}");
+
+            // Пробел между выводами значений массива и коллекции, чтобы все не смешалось в кучу
+            Console.WriteLine("\n");
+
+            /* Если в коллекции uniqueList есть хотя бы одна цифра,
+            то выводим коллекцию, иначе выводим сообщение о том, что коллекция пустая */
+            if (uniqueList.Count > 0)
+                // Выводим все значения из коллекции uniqueList
+                for (int i = 0; i < uniqueList.Count; i++)
+                    Console.WriteLine($"uniqueList - {uniqueList[i]}");
+            else
+                Console.WriteLine("Уникальных чисел не найдено!");
+
+            // Пауза чтобы посмотреть результат, иначе консоль быстро закроется
+            Console.WriteLine("Чтобы закончить нажмите любую клавишу...");
+            Console.ReadLine();
+        }
+
+        public static List<int> getUniqueNumbers(int[] sortableArr)
+        {
+            // Локальная коллекция для хранения уникальных значений функции
+            List<int> resultList = new List<int>();
+
+            // Проверка, что массив не пустой
+            if (sortableArr.Length > 0)
+            {
+                for (int i = 0; i < sortableArr.Length; i++)
+                {
+                    // Если данное число не находится в локальной коллекции, то обнуляем count и записываем его в num
+                    if (resultList.IndexOf(sortableArr[i]) == -1)
+                    {
+                        num = sortableArr[i];
+                        count = 0;
+                    }
+
+                    // "Пробегаем" массив считая количество вхождений проверяемого числа в массив
+                    for (int j = 0; j < sortableArr.Length; j++)
+                        if (sortableArr[j] == num)
+                            count++;
+
+                    // Если число встречено лишь один раз в массиве, то сохраняем его
+                    if (count == 1)
+                        resultList.Add(num);
+                }
+            }
+
+            /* Возвращаем коллекцию уникальных чисел, если переданный в функцию массив был пустой,
+            то и коллекция будет пустой*/
+            return resultList;
+        }
+    }
+}
---
+++ Задание 2/task2.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 07 2022 г., 12:23
+-- Версия сервера: 8.0.24
+-- Версия PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `task2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `departament`
+--
+
+CREATE TABLE `departament` (
+  `ID` int NOT NULL,
+  `NAME` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `departament`
+--
+
+INSERT INTO `departament` (`ID`, `NAME`) VALUES
+(4, 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `employee`
+--
+
+CREATE TABLE `employee` (
+  `ID` int NOT NULL,
+  `DEPARTAMENT_ID` int NOT NULL,
+  `CHIEF_ID` int NOT NULL,
+  `NAME` varchar(100) NOT NULL,
+  `SALARY` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `employee`
+--
+
+INSERT INTO `employee` (`ID`, `DEPARTAMENT_ID`, `CHIEF_ID`, `NAME`, `SALARY`) VALUES
+(1, 1, 1, 'Ilya', 25000),
+(2, 2, 1, 'Ivan', 30000),
+(3, 3, 1, 'Andrey', 70000);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
